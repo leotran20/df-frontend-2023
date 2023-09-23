b8ade6cece9643f029f902d34248ee4146448692
Leo Tran <thienlinh1420@gmail.com>
2023-09-23 17:03:05 +1000

    implement pagination and dark mode

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -10,18 +10,25 @@ const Action = {
 }
 
 const dataKey = 'books';
+const limit = 5;
 let action, selectedItem;
 
 function App() {
+    const [darkMode, setDarkMode] = React.useState(false);
     const [modalIsOpen, setIsOpen] = React.useState(false);
     const [data, setData] = React.useState([]);
     const [searchKey, setSearchKey] = React.useState('');
+    const [page, setPage] = React.useState(0);
+    const [total, setTotal] = React.useState(0);
 
     useEffect(() => {
         const bookList = JSON.parse(localStorage.getItem(dataKey) || '[]');
         setData(bookList);
-        console.log(bookList)
     }, []);
+
+    useEffect(() => {
+        calculatePages();
+    }, [data, searchKey]);
 
     function openModal(selectedAction, item) {
         selectedItem = item;
@@ -47,6 +54,7 @@ function App() {
     function updateData(data) {
         localStorage.setItem(dataKey, JSON.stringify(data));
         setData(data);
+        setPage(0);
     }
 
     function addItem(newItem) {
@@ -66,8 +74,41 @@ function App() {
         setIsOpen(false);
     }
 
+    function filterData() {
+       return data.filter(i => i.name.toLowerCase().includes(searchKey.toLowerCase()));
+    }
+
+    function calculatePages() {
+        const filteredData = filterData();
+        const numberOfPages = Math.ceil((filteredData.length) / limit);
+        setTotal(numberOfPages);
+    }
+
+    function renderItems() {
+        const filteredData = filterData();
+        const pagedData = filteredData.slice(page * limit, (page + 1) * limit);
+        return pagedData.map(i => {
+            return (
+                <tr key={i.name}>
+                    <td colSpan={2}>{i.name}</td>
+                    <td>{i.author}</td>
+                    <td>{i.topic}</td>
+                    <td>
+                        <button className="btn btn-link" type="button"
+                                onClick={() => openModal(Action.DELETE, i)}><span>Delete</span></button>
+                    </td>
+                </tr>
+            )
+        });
+    }
+
+    function toggleDarkMode() {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle('dark');
+    }
+
     return (
-        <div id="App" className="App">
+        <div className={"App"}>
             <header>
 
                 <a className="brand-name">
@@ -75,7 +116,14 @@ function App() {
                 </a>
 
                 <div className="profile">
-        <span className="material-symbols-outlined">
+                    <div className='dark-mode-toggle'>
+                        <div className={`toggle-switch ${darkMode ? 'on' : 'off'}`}
+                             onClick={() => toggleDarkMode()}>
+                            <div className="slider"></div>
+                        </div>
+                        <span>{`${darkMode ? 'Light' : 'Dark'}`} Mode</span>
+                    </div>
+                    <span className="material-symbols-outlined">
 account_circle
 </span>
                     <span>Leo Tran</span>
@@ -94,26 +142,21 @@ account_circle
                     <table className="main-content">
                         <tbody>
                         <tr>
-                            <th colSpan="2">Name</th>
+                            <th colSpan={2}>Name</th>
                             <th>Author</th>
                             <th>Topic</th>
                             <th>Action</th>
                         </tr>
-                        {data.filter(i => i.name.toLowerCase().includes(searchKey.toLowerCase())).map(i => {
-                            return (
-                                <tr key={i.name}>
-                                    <td colSpan="2">{i.name}</td>
-                                    <td>{i.author}</td>
-                                    <td>{i.topic}</td>
-                                    <td>
-                                        <button className="btn btn-link" type="button"
-                                                onClick={() => openModal(Action.DELETE, i)}><span>Delete</span></button>
-                                    </td>
-                                </tr>
-                            )
-                        })}
+                        {renderItems()}
                         </tbody>
                     </table>
+                    <div className="pagination">
+                        {
+                            Array.from({length: total}, (v, i) => <button
+                                className={"btn btn-link" + (i === page ? ' active' : '')}
+                                type="button" onClick={() => setPage(i)} key={i}>{i + 1}</button>)
+                        }
+                    </div>
                 </div>
             </main>
 
